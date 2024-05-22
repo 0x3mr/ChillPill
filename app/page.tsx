4344962a0cfa09ad3ff57ac7df71dfa590e2790e
madiocre <wolvirex@gmail.com>
2024-05-22 20:03:25 +0300

    attempt to fix style

--- app/page.tsx
+++ app/page.tsx
@@ -23,11 +23,14 @@ export default function Home() {
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-pink-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:pink:to-pink-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
       </div>
+      <div>
+      <p className="fixed left-0 top-0 flex w-full justify-center">
+          Click any song to play
+        </p>
+      </div>
 
       <div className="flex flex-grow items-center justify-center w-full">
         <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-          <div>
-            <p>Click anything to play</p>
             {sounds.length > 0 ? (
               sounds.map((soundItem) => (
                 <Button
@@ -40,7 +43,6 @@ export default function Home() {
             ) : (
               <p>No sounds available</p>
             )}
-          </div>
         </div>
       </div>
     </main>
