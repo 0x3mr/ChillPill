bfe3f8e4486e51fd89329fec5a78d711dc97b56a
justxd22 <xdjust18@gmail.com>
2024-05-30 15:00:06 +0300

    fix types error

--- pull_run.sh
+++ pull_run.sh
@@ -3,5 +3,6 @@
 cd /home/ubuntu/ChillPill
 git pull
 npm --prefix /home/ubuntu/ChillPill install
+npm --prefix /home/ubuntu/ChillPill i --save-dev @types/howler
 npm --prefix /home/ubuntu/ChillPill run build
 pm2 restart chillpill
