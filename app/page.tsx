96dee7a72000d2ec7405d9872efe1641e85ace1f
justxd22 <xdjust18@gmail.com>
2024-06-05 00:05:10 +0300

    fix large padding

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
   };
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"> </div>
       <img src={logo} className='logo'/>
